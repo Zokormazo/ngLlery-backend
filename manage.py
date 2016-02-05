0b7f0373f46678ea3bc6b33297a6092b61722448
Julen Landa Alustiza <julen@zokormazo.info>
2016-02-05 19:08:21 +0100

    add CORS headers

--- manage.py
+++ manage.py
@@ -18,6 +18,14 @@ if path.exists('.env'):
 
 # create app, flask-script manager and flask-migrate migrate objects
 app = create_app(getenv('FLASK_CONFIG') or 'default')
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
+
 manager = Manager(app)
 migrate = Migrate(app,db)
 
