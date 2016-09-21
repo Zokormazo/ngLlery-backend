6bec177726bc201159918fd248cac95acb342390
Julen Landa Alustiza <julen@zokormazo.info>
2016-09-22 01:17:10 +0200

    add missing test units

--- tests/test_api_dashboard_user_roles_resource.py
+++ tests/test_api_dashboard_user_roles_resource.py
@@ -137,3 +137,102 @@ class ApiDashboardUserRolesResourceTestCase(unittest.TestCase):
     def test_dashboard_user_roles_resource_put(self):
         response = self.client.put(api.url_for(DashboardUserRolesResource,user_id=0,role_name='Role'), headers=self.get_headers())
         self.assertEqual(response.status_code,405)
+
+    def test_dashboard_user_roles_resource_delete_without_token(self):
+        response = self.client.delete(api.url_for(DashboardUserRolesResource, user_id=0, role_name='Role'), headers=self.get_headers())
+        self.assertEqual(response.status_code,401)
+
+    def test_dashboard_user_roles_resource_delete_with_valid_token_and_valid_user_id_and_role_name(self):
+        r = Role(name='admin')
+        r1 = Role(name='poweruser')
+        u = User(username='test', email='test@test.com')
+        u.set_password('test')
+        u.roles.append(r)
+        db.session.add(r)
+        db.session.add(r1)
+        db.session.add(u)
+        db.session.commit()
+        response = self.client.post(api.url_for(LoginResource),
+                                    data=json.dumps({'username': 'test', 'password': 'test'}),
+                                    headers=self.get_headers())
+        json_data =  json.loads(response.data.decode('utf-8'))
+        token = json_data['token']
+        user_id = json_data['user']['id']
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=user_id, role_name='poweruser'),
+                                    data=json.dumps({}),
+                                    headers=self.get_headers(token))
+        self.assertEqual(response.status_code,204)
+
+    def test_dashboard_user_roles_resource_delete_with_valid_token_and_role_and_valid_user_id_but_invalid_role_name(self):
+        r = Role(name='admin')
+        u = User(username='test', email='test@test.com')
+        u.set_password('test')
+        u.roles.append(r)
+        db.session.add(r)
+        db.session.add(u)
+        db.session.commit()
+        response = self.client.post(api.url_for(LoginResource),
+                                    data=json.dumps({'username': 'test', 'password': 'test'}),
+                                    headers=self.get_headers())
+        json_data =  json.loads(response.data.decode('utf-8'))
+        token = json_data['token']
+        user_id = json_data['user']['id']
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=user_id, role_name='Role'),
+                                    data=json.dumps({}),
+                                    headers=self.get_headers(token))
+        self.assertEqual(response.status_code,404)
+
+    def test_dashboard_user_roles_resource_delete_with_valid_token_and_role_and_invalid_user_id_and_valid_role_name(self):
+        r = Role(name='admin')
+        u = User(username='test', email='test@test.com')
+        u.set_password('test')
+        u.roles.append(r)
+        db.session.add(r)
+        db.session.add(u)
+        db.session.commit()
+        response = self.client.post(api.url_for(LoginResource),
+                                    data=json.dumps({'username': 'test', 'password': 'test'}),
+                                    headers=self.get_headers())
+        json_data =  json.loads(response.data.decode('utf-8'))
+        token = json_data['token']
+        user_id = json_data['user']['id']
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=user_id+1, role_name='admin'),
+                                    data=json.dumps({}),
+                                    headers=self.get_headers(token))
+        self.assertEqual(response.status_code,404)
+
+    def test_dashboard_user_roles_resource_delete_with_valid_token_and_role_and_invalid_user_id_and_role_name(self):
+        r = Role(name='admin')
+        u = User(username='test', email='test@test.com')
+        u.set_password('test')
+        u.roles.append(r)
+        db.session.add(r)
+        db.session.add(u)
+        db.session.commit()
+        response = self.client.post(api.url_for(LoginResource),
+                                    data=json.dumps({'username': 'test', 'password': 'test'}),
+                                    headers=self.get_headers())
+        json_data =  json.loads(response.data.decode('utf-8'))
+        token = json_data['token']
+        user_id = json_data['user']['id']
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=user_id+1, role_name='Role'),
+                                    data=json.dumps({}),
+                                    headers=self.get_headers(token))
+        self.assertEqual(response.status_code,404)
+
+    def test_dashboard_user_roles_resource_delete_with_valid_token_but_without_role(self):
+        u = User(username='test', email='test@test.com')
+        u.set_password('test')
+        db.session.add(u)
+        db.session.commit()
+        response = self.client.post(api.url_for(LoginResource),
+                                    data=json.dumps({'username': 'test', 'password': 'test'}),
+                                    headers=self.get_headers())
+        json_data =  json.loads(response.data.decode('utf-8'))
+        token = json_data['token']
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=0, role_name='Role'), headers=self.get_headers(token))
+        self.assertEqual(response.status_code,403)
+
+    def test_dashboard_user_roles_resource_delete_with_invalid_token(self):
+        response = self.client.delete(api.url_for(DashboardUserRolesResource,user_id=0,role_name='Role'), headers=self.get_headers('THIS_IS_AN_INVALID_TOKEN'))
+        self.assertEqual(response.status_code,401)
